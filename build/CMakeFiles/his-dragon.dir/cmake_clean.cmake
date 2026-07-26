bc149d23249ce3c0b4883ec27e08571ab1325004
long <longkute169@gmail.com>
2026-07-26 09:57:58 +0700

    set up project

---
+++ build/CMakeFiles/his-dragon.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/his-dragon.dir/link.d"
+  "CMakeFiles/his-dragon.dir/src/main.cpp.o"
+  "CMakeFiles/his-dragon.dir/src/main.cpp.o.d"
+  "his-dragon"
+  "his-dragon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/his-dragon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
